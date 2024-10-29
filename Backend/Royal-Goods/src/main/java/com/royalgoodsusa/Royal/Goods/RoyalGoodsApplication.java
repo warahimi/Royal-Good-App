a0993b98b53936cf6b7bf8e99169e4858103067d
Wahidullah Rahimi <wahidrahimi45@gmail>
2024-10-28 22:36:00 -0700

    Connected Backed to AWS S3

--- Backend/Royal-Goods/src/main/java/com/royalgoodsusa/Royal/Goods/RoyalGoodsApplication.java
+++ Backend/Royal-Goods/src/main/java/com/royalgoodsusa/Royal/Goods/RoyalGoodsApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+/*
+	Royal Goods Application
+ */
 @SpringBootApplication
 @EnableJpaAuditing // Enables JPA Auditing
 public class RoyalGoodsApplication {
